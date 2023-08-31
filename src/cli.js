6c5243f5f6a4b01de6f78baf435029744d7adafa
mxgdt <ctcceramics@mail.ru>
2023-08-31 03:15:23 +0300

    added user greeting and user name question

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const welcome = (text) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(text);
+  return userName;
+};
+
+export default welcome;
